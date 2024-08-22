b6cb5b0bfb5f894e411a14ca0468d69f87d6d7cb
hyewon <115865476+won-hub@users.noreply.github.com>
2024-08-22 09:55:24 +0900

    Update SampleApplicationTests.java

--- src/test/java/com/test/sample/SampleApplicationTests.java
+++ src/test/java/com/test/sample/SampleApplicationTests.java
@@ -2,6 +2,7 @@ package com.test.sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SampleApplicationTests {
@@ -24,4 +25,14 @@ class SampleApplicationTests {
 		assertEquals(2, result);  // 5 - 3 = 2 여야 테스트 통과
 	}
 
+}
+
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
 }
