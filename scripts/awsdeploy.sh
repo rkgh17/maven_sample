0ef3e6140359ae4fdb9774bcd121a246b089c3cd
hjh <rkgh17@gmail.com>
2024-08-22 13:23:17 +0900

    -

--- scripts/awsdeploy.sh
+++ scripts/awsdeploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f /home/ubuntu/mvn/apache-tomcat-10.1.4/webapps/sample-0.0.1-SNAPSHOT.war ]; then
+    rm /home/ubuntu/mvn/apache-tomcat-10.1.4/webapps/sample-0.0.1-SNAPSHOT.war
+fi
+
 echo "톰캣 중지"
 /home/ubuntu/mvn/apache-tomcat-10.1.4/bin/shutdown.sh;
 
