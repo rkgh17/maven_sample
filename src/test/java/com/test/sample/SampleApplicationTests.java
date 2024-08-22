59be5df8d0604b9317531899ca6a2d6cdbc14c67
hyewon <115865476+won-hub@users.noreply.github.com>
2024-08-22 09:47:04 +0900

    Update SampleApplicationTests.java

--- src/test/java/com/test/sample/SampleApplicationTests.java
+++ src/test/java/com/test/sample/SampleApplicationTests.java
@@ -9,5 +9,19 @@ class SampleApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void additionTest() {
+		Calculator calc = new Calculator();
+		int result = calc.add(2, 3);
+		assertEquals(5, result);  // 2 + 3 = 5 여야 테스트 통과
+	}
+	
+	@Test
+	void subtractionTest() {
+		Calculator calc = new Calculator();
+		int result = calc.subtract(5, 3);
+		assertEquals(2, result);  // 5 - 3 = 2 여야 테스트 통과
+	}
 
 }
